93417b4f4e15c04859314eea8b623c225b4cfa14
hellowangsai <hellowangsai@qq.com>
2018-08-09 16:57:37 +0800

    Create intr_pthread_join.c
    测试pthread_timedjoin_np是否是可中断的sys call。
    实验后，发现不会被中断，而且errno仍保持为0，不会被设置为EINTR(4)；相反 sleep就会被中断，且设置errno。

---
+++ intr_pthread_join.c
@@ -0,0 +1,46 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+
+int stop = 0;
+void* f(void* aaa) {
+    printf("in f, pid: %d", pthread_self());
+    while (stop == 0) {
+       sleep(6);
+       printf("f() errno: %d\n", errno);
+       fflush(stdout);
+    }
+    sleep(10);
+    return NULL;
+}
+
+void handle_signal(int signal) {
+    stop = 1;
+    printf("in signal, pid: %d\n", pthread_self()); // in my test, pid is same as main.
+    fflush(stdout);
+}
+
+int main() {
+    struct sigaction sa;
+    sa.sa_handler = &handle_signal;
+    sa.sa_flags = SA_RESTART;
+    sigfillset(&sa.sa_mask);
+    sigaction(SIGINT, &sa, NULL);
+    pthread_t thread;
+    int ret;
+    ret = pthread_create(&thread, NULL, f, NULL);
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    ret = -1;
+    printf("in main, pid: %d\n", pthread_self());
+    fflush(stdout);
+    while (ret != 0) {
+        ts.tv_sec += 5;
+        ret = pthread_timedjoin_np(thread, NULL, &ts);
+        printf("ret code: %d, errno: %d\n", ret, errno);
+        // sleep(5); // sleep can be interrupt, and set errno to EINTR.
+    }
+}

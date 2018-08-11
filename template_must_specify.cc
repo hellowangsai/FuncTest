c9734b6f98874e275a6cf1687888a36080967cb9
hellowangsai <hellowangsai@qq.com>
2018-08-12 02:05:33 +0800

    Update template_must_specify.cc

--- template_must_specify.cc
+++ template_must_specify.cc
@@ -1,12 +1,15 @@
+// g++ -std=c++14 template_must_specify.cc
+// view there is compile error
+
+// variable template since c++14
+// an alternative is use class template contains static value, like as std::integral_constant.
 template <class Cls>
-struct False {
-    static constexpr bool value = false;
-};
+constexpr bool FalseValue = false;
 
 template <class Cls>
 struct MustSpecify {
     static_assert(false, "this class template Must Specify"); // compile error, must be a false depends template param `Cls`
-    static_assert(False<Cls>::value, "this class template Must Specify"); // compile pass
+    static_assert(FalseValue<Cls>, "this class template Must Specify"); // compile pass
 };
 
 template <>
